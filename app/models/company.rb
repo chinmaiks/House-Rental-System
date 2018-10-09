5eebbddb095238c6f57439358b0fae8fecb9a24a
Harsh Shah <harshshah949494@gmail.com>
2018-10-09 19:48:26 -0400

    Model changes for cascading deletes to work. Company left

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
-  has_many :users
-  has_many :houses
+  has_and_belongs_to_many :user, :dependent => :destroy
+  has_many :houses, :dependent => :destroy
   validates :name, :website, :address, :size, :founded_in, :revenue, :synopsis, presence: true, allow_blank: false
   validates :revenue, :size, :founded_in, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :house
-  has_many :company
+  has_many :house, :dependent => :destroy
+  has_and_belongs_to_many :company, :dependent => :destroy
   validates :name, :role, :email, :presence=>true
 end
