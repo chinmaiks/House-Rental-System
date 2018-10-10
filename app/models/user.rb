73d7346289f2c49af5341905663782f4ce6a3da8
Hiral Singadia <hasingad@ncsu.edu>
2018-10-09 23:46:14 -0400

    inquiry added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :house, :dependent => :destroy
-  has_and_belongs_to_many :company, :dependent => :destroy
+  has_many :company, :dependent => :destroy
   validates :name, :role, :email, :presence=>true
 end
